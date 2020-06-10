81b8ac1e10a094ec9a7d7e6e3025503b299f8803
Graham Mothersill <graham.mothersill@gmail.com>
2020-06-10 20:56:32 +0000

    implementing new user IDs

--- express_server.js
+++ express_server.js
@@ -1,17 +1,46 @@
 const express = require("express"); // <<----- and this
 const app = express(); // <<------------------ REMEMBER THIS
 const PORT = 8080; // default port 8080
-var morgan = require('morgan');
-
-
+const morgan = require('morgan');
 app.use(morgan('combined'));
 app.set("view engine", "ejs")
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+function addNewUser(email, password, userId) {
+  const newUser = {
+    id: userId,
+    email,
+    password
+  }
+  users[userId] = newUser;
+
+  return userId;
+};
 
 function generateRandomString() {
   let characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -21,21 +50,16 @@ function generateRandomString() {
     string += characters[ranNumb];
   }
   return string;
-}
-
-generateRandomString();
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-var cookieParser = require('cookie-parser');
-app.use(cookieParser());
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] }
+  const userId = req.cookies['user_id']
+  const currentUser = users[userId];
+  let templateVars = { user: currentUser }
   res.render("urls_new", templateVars);
 });
 
@@ -55,7 +79,9 @@ app.post("/urls/:shortURL", (req, res) => {
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] }
+  const userId = req.cookies['user_id']
+  const currentUser = users[userId];
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: currentUser }
   res.render("urls_show", templateVars);
 });
 
@@ -73,14 +99,18 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const userId = req.cookies['user_id']
+  const currentUser = users[userId];
+  let templateVars = { urls: urlDatabase, user: currentUser };
   res.render("urls_index", templateVars);
   // ADD TO OTHER ROUTES
 });
 
 
 app.get("/u/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
+  const userId = req.cookies['user_id']
+  const currentUser = users[userId];
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: currentUser };
   //res.render("urls_show", templateVars);
   //console.log("UM TEST");
   res.redirect(templateVars.longURL);
@@ -100,15 +130,27 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-
   res.clearCookie("username");
   // Implement the /logout endpoint so that it clears the username cookie and redirects the user back to the /urls page.
   res.redirect("/urls")
 });
 
 app.get("/register", (req, res) => {
-  let templateVars = { username: req.cookies["username"] }
+  const userId = req.cookies['user_id'];
+  const currentUser = users[userId];
+  let templateVars = { user: currentUser };
   res.render("register", templateVars)
+});
+
+app.post("/register", (req, res) => {
+  let email = req.body.email
+  let password = req.body.password
+  let newID = generateRandomString()
+  addNewUser(email, password, newID)
+
+  console.log(users)
+  res.cookie("user_id", newID);
+  res.redirect("/urls")
 });
 
 app.get("/hello", (req, res) => {
